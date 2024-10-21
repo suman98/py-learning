aaeb41f440d2b898626ff00c20969f3d05cf6e01
suman98 <41943773+suman98@users.noreply.github.com>
2024-10-21 14:36:44 +0545

    init

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import View
+from django.http import HttpResponse
+
+class HelloWorldView(View):
+    def get(self, request):
+        return HttpResponse("Hello, World!")
